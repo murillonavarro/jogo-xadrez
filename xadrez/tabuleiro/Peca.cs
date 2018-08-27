2de1a5443b481b9cbb76f186566a770bbe92de22
Murillo Navarro <murillobilchesnavarro@gmail.com>
2018-08-27 17:06:12 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez/tabuleiro/Peca.cs
+++ xadrez/tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
 
         public Posicao posicao { get; set;}
@@ -15,10 +15,10 @@
             this.cor = cor;
             this.qteMovimentos = 0;
         }
-
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
         }
+        public abstract bool[,] movimentosPossiveis();
     }
 }
